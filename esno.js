10082161772f2e6c64892d95516cb5b0d956687b
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-05 15:08:52 +0800

    chore: fix

--- esno.js
+++ esno.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
+const { execSync, execFileSync } = require('child_process')
 const path = require('path')
+const { existsSync } = require('fs')
 
-execSync(
-  `node -r ${path.join(
-    __dirname,
-    'node_modules',
-    'esm',
-    'index.js'
-  )} -r ${path.join(
-    __dirname,
-    'node_modules',
-    'esbuild-register',
-    'register.js'
-  )} ${process.argv
-    .slice(2)
-    .map((i) => `"${i}"`)
-    .join(' ')}`,
-  { stdio: 'inherit' }
-)
+const local = existsSync(path.join(__dirname, 'node_modules'))
+
+const esm = local
+  ? path.join(__dirname, 'node_modules', 'esm', 'index.js')
+  : path.join(__dirname, '..', 'esm', 'index.js')
+
+const register = local
+  ? path.join(__dirname, 'node_modules', 'esm', 'index.js')
+  : path.join(__dirname, '..', 'esbuild-register', 'register.js')
+
+const argv = process.argv
+  .slice(2)
+  .map((i) => `"${i}"`)
+  .join(' ')
+
+execSync(`node -r ${esm} -r ${register} ${argv}`, { stdio: 'inherit' })
