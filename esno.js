3073b2846e0838e99673528a0b305893cc96657e
Anthony Fu <anthonyfu117@hotmail.com>
2020-08-06 16:25:38 +0800

    refactor: improve resolving

--- esno.js
+++ esno.js
@@ -1,16 +1,9 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process')
-const { existsSync } = require('fs')
-const path = require('path')
 
-const esm = existsSync(path.join(__dirname, 'node_modules', 'esm'))
-  ? path.join(__dirname, 'node_modules', 'esm', 'index.js')
-  : path.join(__dirname, '..', 'esm', 'index.js')
-
-const register = existsSync(path.join(__dirname, 'node_modules', 'esbuild-register'))
-  ? path.join(__dirname, 'node_modules', 'esm', 'index.js')
-  : path.join(__dirname, '..', 'esbuild-register', 'register.js')
+const esm = require.resolve('esm')
+const register = require.resolve('esbuild-register')
 
 const argv = process.argv
   .slice(2)
