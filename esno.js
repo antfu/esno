443e63a68c49c951bd0d92fc101b12b993dc37da
Anthony Fu <anthonyfu117@hotmail.com>
2020-10-13 19:12:39 +0800

    chore: revert "feat: support type: module"
    This reverts commit b78aef4114712af04401a491a7d8be9efa8a8a8e.

--- esno.js
+++ esno.js
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process')
-const fs = require('fs')
 
-let isModule = false
-try {
-  const { type } = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
-  isModule = type === 'module'
-} catch {}
+const esm = require.resolve('esm')
+const register = require.resolve('esbuild-register')
 
 const argv = process.argv
   .slice(2)
   .map((i) => `"${i}"`)
   .join(' ')
 
-const register = require.resolve('esbuild-register')
-
-if (isModule) {
-  execSync(`node -r ${register} ${argv}`, { stdio: 'inherit' })
-} else {
-  const esm = require.resolve('esm')
-  execSync(`node -r ${esm} -r ${register} ${argv}`, { stdio: 'inherit' })
-}
+execSync(`node -r ${esm} -r ${register} ${argv}`, { stdio: 'inherit' })
