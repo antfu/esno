b78aef4114712af04401a491a7d8be9efa8a8a8e
Anthony Fu <anthonyfu117@hotmail.com>
2020-10-13 19:02:57 +0800

    feat: support type: module

--- esno.js
+++ esno.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process')
+const fs = require('fs')
 
-const esm = require.resolve('esm')
-const register = require.resolve('esbuild-register')
+let isModule = false
+try {
+  const { type } = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+  isModule = type === 'module'
+} catch {}
 
 const argv = process.argv
   .slice(2)
   .map((i) => `"${i}"`)
   .join(' ')
 
-execSync(`node -r ${esm} -r ${register} ${argv}`, { stdio: 'inherit' })
+const register = require.resolve('esbuild-register')
+
+if (isModule) {
+  execSync(`node -r ${register} ${argv}`, { stdio: 'inherit' })
+} else {
+  const esm = require.resolve('esm')
+  execSync(`node -r ${esm} -r ${register} ${argv}`, { stdio: 'inherit' })
+}
