876de37bb64395263eebffecef625fa95ffb49e0
hiroki <hiroki.osame@gmail.com>
2021-03-01 01:46:59 -0500

    fix: allow special characters in arguments

--- esno.js
+++ esno.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process')
+const { spawnSync } = require('child_process')
 
 const register = require.resolve('esbuild-register')
 
-const argv = process.argv
-  .slice(2)
-  .map((i) => `"${i}"`)
-  .join(' ')
+const argv = process.argv.slice(2)
 
-execSync(`node -r ${register} ${argv}`, { stdio: 'inherit' })
+spawnSync('node', ['-r', register, ...argv], { stdio: 'inherit' })
