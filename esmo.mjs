8d9bc5a4c56ba9439d8803d6185f2405982e015c
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-14 11:26:04 +0800

    fix(esmo): use bare `--loader` argument

--- esmo.mjs
+++ esmo.mjs
@@ -6,5 +6,5 @@ const spawnSync = spawn.sync
 
 const argv = process.argv.slice(2)
 resolve('esbuild-node-loader', import.meta.url).then((path) => {
-    process.exit(spawnSync('node', ['--experimental-loader', path, ...argv], { stdio: 'inherit' }).status)
+  process.exit(spawnSync('node', ['--loader', path, ...argv], { stdio: 'inherit' }).status)
 })
